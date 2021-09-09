63b83c9465f66aec9bb8cc63eaf619a141ed02ac
Frank Parra <panf9188@gmail.com>
2021-09-08 20:40:18 -0500

    Agregamos la clase repartidor por primera vez

---
+++ Repartidor.php
@@ -0,0 +1,22 @@
+<?php
+
+class Cocinero{
+
+    public  $apellidos;
+    public  $nombre;
+    public  $dni;
+    public  $direccion;
+    public  $celular;
+    public  $fecha_nacimiento;
+    public  $especialidad;
+    public  $horario;
+
+    public function nombreCompleto(){
+        return $this->nombres . " " . $this->apellidos;
+    }
+
+    public function saludoCumpleanios(){
+        return "Feliz cumpleaños" . $this->nombreCompleto();
+    }
+
+}
