bbbd3ca732e69e9410d1dc39890cd8b9ba943f3a
Frank Parra <panf9188@gmail.com>
2021-09-09 12:52:16 -0500

    Creamos una funciÂón similar a la de la clase padre pero con algo más. Sobre escritura

--- Cocinero.php
+++ Cocinero.php
@@ -6,6 +6,11 @@ class Cocinero extends Persona {
     //Con poner la palabra clase extends y la clase padre, todos los métodos y atributos de la clase padre se heredan
 
     public  $especialidad;
-    public  $horario;    
+    public  $horario;  
+    
+    public function saludoCumpleanios(){
+        //Esto se conoce sobre escritura, ya que esta función existe en la clase padre
+        return "Feliz cumpleaños al mejor cocinero " . $this->nombreCompleto();
+    }
 
 }
